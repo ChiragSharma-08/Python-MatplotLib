38c7b0e2b312daf63efe77b8cf8dc65404de8ed3
Chirag-Sharma-08 <257071564+ChiragSharma-08@users.noreply.github.com>
2026-06-21 21:05:02 +0530

    Intro
    In this commit we started working with Matplotlib.

---
+++ Intro.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [2023, 2024, 2025, 2026] # we can also make numpy arrays instead of a list
+y = [15, 25, 30, 20]
+
+plt.plot(x,y)
+# plt.plot(x)
+# plt.plot(y)
+
+plt.show()
