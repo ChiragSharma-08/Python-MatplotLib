a03d4d2b120300c8f8330cd54ad00b14927f8796
Chirag-Sharma-08 <257071564+ChiragSharma-08@users.noreply.github.com>
2026-06-25 02:12:31 +0530

    Pandas  with matplotlib
    In this commit we learn about how we can use pandas to read and use data for our graph.

--- Subplots.py
+++ Subplots.py
@@ -1,25 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-# Figure = THe entire canvas
-# Ax = A single plot (subplot)
+df = pd.read_csv("pokemon.csv")
 
-x = np.array([1,2,3,4,5])
+type_count = df["Type1"].value_counts(ascending= True)
 
-# print(plt.subplot(2,2))
-figure, axes = plt.subplots(2,2) # Creating 2X2 subplots 2 row, 2 column
-
-axes[0,0].plot(x, x*2, color = "red")
-axes[0,0].set_title("x*2")
-
-axes[0,1].plot(x, x**2, color = "blue")
-axes[0,1].set_title("x^2")
-
-axes[1,0].plot(x, x**3, color = "green")
-axes[1,0].set_title("x^3")
-
-axes[1,1].plot(x, 2**x, color = "#948336")
-axes[1,1].set_title("2^x")
+plt.barh(type_count.index, type_count.values, color= "purple",
+                                              edgecolor= "black")
+plt.title("No. of Pokemon by primary type")
+plt.xlabel("Count")
+plt.ylabel("Type")
 
 plt.tight_layout()
 plt.show()
